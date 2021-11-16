29986da0ec5d109f66123190e9577f828d3c523f
Ohdaejin <xla3095@gmail.com>
2021-11-16 12:51:05 +0900

    명령어 작업

--- src/com/odj/exam/Board/Main.java
+++ src/com/odj/exam/Board/Main.java
@@ -1,11 +1,19 @@
 package com.odj.exam.Board;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("==게시판 v 0.1==");
-        System.out.println("==프로그램 시작==");
-        System.out.println("==프로그램 종료=");
-    }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String a;
+    a = sc.nextLine();
+
+    System.out.println("==게시판 v 0.1==");
+    System.out.println("==프로그램 시작==");
+    System.out.println("명령) " + a);
+    System.out.println("입력된 명령어 : " +a );
+    System.out.println("==프로그램 종료=");
+  }
 }
