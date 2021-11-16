634d52bca95eca63bb67c47c6fc5520ccf5991c8
Ohdaejin <xla3095@gmail.com>
2021-11-16 13:21:12 +0900

    while문 사용 및 종료

--- src/com/odj/exam/Board/Main.java
+++ src/com/odj/exam/Board/Main.java
@@ -7,13 +7,21 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    String a;
-    a = sc.nextLine();
-
     System.out.println("==게시판 v 0.1==");
     System.out.println("==프로그램 시작==");
-    System.out.println("명령) " + a);
-    System.out.println("입력된 명령어 : " +a );
+
+    while (true) {
+      System.out.println("명령) ");
+      String cmd = sc.nextLine();
+
+      if (cmd.equals("exit")){
+        break;
+      }
+
+      System.out.printf("입력된 명령어 : %s\n", cmd);
+    }
+
     System.out.println("==프로그램 종료=");
   }
 }
+
