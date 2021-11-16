2be3662e04c843dd5786299cdcf453d5aa8643a9
Ohdaejin <xla3095@gmail.com>
2021-11-16 12:32:13 +0900

    첫 세팅

---
+++ src/com/odj/exam/Board/Main.java
@@ -0,0 +1,11 @@
+package com.odj.exam.Board;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("==게시판 v 0.1==");
+        System.out.println("==프로그램 시작==");
+        System.out.println("==프로그램 종료=");
+    }
+}
