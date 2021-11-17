4c4b2dfb6c3771092b8c6c626b34bb4f0e1a33c0
Ohadaejin <xla3095@gmail.com>
2021-11-17 09:54:06 +0900

    클론

--- src/com/odj/exam/Board/Main.java
+++ src/com/odj/exam/Board/Main.java
@@ -14,14 +14,24 @@ public class Main {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
 
-      if (cmd.equals("exit")){
+      if (cmd.equals("exit")) {
         break;
+      } else if (cmd.equals("usr/article/write"))
+        System.out.println("= 게시물 등록=");
+      System.out.println("제목 : ");
+      String title = sc.nextLine();
+      System.out.println("내용 : ");
+      String body = sc.nextLine();
+      int id = 1;
+      System.out.printf("%d 번 게시물이 입력되었습니다.\n", id);
+      {
+        else{
+      }
+        System.out.printf("입력된 명령어 : %s\n", cmd);
       }
 
-      System.out.printf("입력된 명령어 : %s\n", cmd);
+      System.out.println("==프로그램 종료=");
     }
-
-    System.out.println("==프로그램 종료=");
   }
 }
 
