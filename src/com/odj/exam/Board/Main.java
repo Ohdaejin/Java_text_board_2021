241e18bf3fc0fd82bbdd2b8803b071451c2fb437
Ohadaejin <xla3095@gmail.com>
2021-11-17 10:08:10 +0900

    게시물 등록1

--- src/com/odj/exam/Board/Main.java
+++ src/com/odj/exam/Board/Main.java
@@ -3,12 +3,11 @@ package com.odj.exam.Board;
 import java.util.Scanner;
 
 public class Main {
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("==게시판 v 0.1==");
-    System.out.println("==프로그램 시작==");
+    System.out.println("== 게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
 
     while (true) {
       System.out.printf("명령) ");
@@ -16,22 +15,21 @@ public class Main {
 
       if (cmd.equals("exit")) {
         break;
-      } else if (cmd.equals("usr/article/write"))
-        System.out.println("= 게시물 등록=");
-      System.out.println("제목 : ");
-      String title = sc.nextLine();
-      System.out.println("내용 : ");
-      String body = sc.nextLine();
-      int id = 1;
-      System.out.printf("%d 번 게시물이 입력되었습니다.\n", id);
-      {
-        else{
-      }
+      } else if (cmd.equals("/usr/article/write")) {
+        System.out.println("- 게시물 등록 -");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        int id = 1;
+        System.out.printf("%d번 게시물이 입력되었습니다.\n", id);
+      } else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
       }
-
-      System.out.println("==프로그램 종료=");
     }
+
+    System.out.println("== 프로그램 종료 ==");
+
+    sc.close();
   }
 }
-
